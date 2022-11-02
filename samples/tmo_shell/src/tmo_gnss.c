253aa0e43ed2721eac9cb314fbbc1501b0ba3de2
John Lange <John.Lange2@T-Mobile.com>
2022-11-02 17:30:14 -0600

    Remove extraneous header

--- samples/tmo_shell/src/tmo_gnss.c
+++ samples/tmo_shell/src/tmo_gnss.c
@@ -16,7 +16,6 @@
 #include <bluetooth/gatt.h>
 #include <zephyr.h>
 #include <drivers/sensor.h>
-#include <drivers/sensor/gnss.h>
 #include <drivers/sensor/cxd5605.h>
 #include <drivers/flash.h>
 #include <device.h>
