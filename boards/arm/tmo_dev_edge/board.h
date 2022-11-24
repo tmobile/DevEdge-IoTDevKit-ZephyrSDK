8624b98990fa5c42decefb27a59f81ab233b0e1a
Dennis Ruffer <Dennis.Ruffer1@T-Mobile.com>
2022-11-23 16:34:18 -0800

    Draft: Integrate SiLabs efr32bg22-initial into all boards
    EFM32PG-STK3402A is in pretty good shape.
    tmo_dev_edge is building and outputting serial
    but getting a USAGE FAULT.
    The others have not been started yet.

--- boards/arm/tmo_dev_edge/board.h
+++ boards/arm/tmo_dev_edge/board.h
@@ -9,12 +9,12 @@
 #define __INC_BOARD_H
 
 /* These pins is used as part of the 9116 initialization routine */
-#define RS9116_GPIO_NAME  "GPIO_A"
+#define RS9116_GPIO_NAME  "gpio@4000a000" /* GPIO_A */
 #define RS9116_POC_GPIO_PIN   8
 #define RS9116_RST_GPIO_PIN   9
 
 /* These pins are used as part of the PAM8904E initialization routine */
-#define BUZZER_ENx_GPIO_NAME  "GPIO_C"
+#define BUZZER_ENx_GPIO_NAME  "gpio@4000a060" /* GPIO_C */
 #define BUZZER_EN1_GPIO_PIN   4
 #define BUZZER_EN2_GPIO_PIN   5
 
@@ -24,7 +24,7 @@
 #define GREEN_LED_GPIO_PIN    9
 #define BLUE_LED_GPIO_PIN     10
 
-#define GNSS_GPIO_NAME "GPIO_F"
+#define GNSS_GPIO_NAME "gpio@4000a0f0" /* GPIO_F */
 #define GNSS_BOOT_REQ 6
 #define GNSS_RESET 8
 #define GNSS_PWR_ON 10
