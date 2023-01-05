2fafe7e827ae96b7fae88334ce3bfb9dd57a778f
jvhutch <18317262+jvhutch@users.noreply.github.com>
2023-01-04 20:06:04 -0700

    Get all PM-supported devices to work

--- samples/wake_sleep/src/main.c
+++ samples/wake_sleep/src/main.c
@@ -4,31 +4,54 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// uart:~$ tmo pm get
+// get: wrong parameter count
+// get - Get a device's power manangement state
+// Subcommands:
+//   murata_1sc
+//   rs9116w@0
+//   pwmleds
+//   sonycxd5605@24
+//   tsl2540@39
+// uart:~$
+
 #include <zephyr/pm/device.h>
 #include <zephyr/pm/device_runtime.h>
 #include <zephyr/pm/state.h>
 
+#if 0
 #if 'y' == CONFIG_WIFI
 #if 'y' == CONFIG_WIFI_RS9116W
 #include <zephyr/drivers/bluetooth/rs9116w.h>
 #endif
 #endif
+#endif
 
-// #include <string.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define TIMER_DURATION K_SECONDS(30)
-#define TIMER_PERIOD   K_SECONDS(30)
+#define TIMER_PERIOD   K_SECONDS(60)
 #define PRIORITY       K_PRIO_COOP(5)
 
 #define TIMER_STOP_FN NULL
 
-// #if 'y' == CONFIG_LOG
 #include <zephyr/logging/log.h>
-// #include <libc/minimal/strerror_table.h>
-// #if 'y' == CONFIG_LOG_MODE_MINIMAL
+#include <libc/minimal/strerror_table.h>
 LOG_MODULE_DECLARE(wake_sleep, CONFIG_PM_LOG_LEVEL);
-// #endif
-// #endif
+
+
+static const char *strerror_extended(int error_value)
+{
+	error_value = abs(error_value);
+	if (sys_nerr < error_value) {
+		return "";
+	} else {
+		return sys_errlist[error_value];
+	}
+}
+
 
 static struct k_thread thread_id;
 void timer_isr(struct k_timer *dummy)
@@ -52,26 +75,28 @@ static void pm_thread(void *this_thread, void *p2, void *p3)
 		enum pm_device_action value;
 		const char *name;
 	} device_action[] = {
+		STRUCT_INIT(PM_DEVICE_ACTION_RESUME),
 		STRUCT_INIT(PM_DEVICE_ACTION_SUSPEND),
 		STRUCT_INIT(PM_DEVICE_ACTION_TURN_OFF),
 		STRUCT_INIT(PM_DEVICE_ACTION_TURN_ON),
-		STRUCT_INIT(PM_DEVICE_ACTION_RESUME),
 	};
 #undef STRUCT_INIT
 
 	const struct device *devices;
 	const size_t n_devices = z_device_get_all_static(&devices);
 	struct {
-		struct device *on_off_support, *resume_suspend_support;
 		struct action {
-			bool discovered, supported;
+			bool probed:1, supported:1;
 		} action[(sizeof device_action / sizeof device_action[0])];
-	} device_action_support[n_devices];
+	} device_info[n_devices];
 
-	memset(&device_action_support, 0, sizeof device_action_support);
+	memset(&device_info, 0, sizeof device_info);
 	for (unsigned char action_index = 0; true;
 	     action_index = (action_index + 1) % (sizeof device_action / sizeof device_action[0])) {
+		LOG_INF("%s(): asleep\n", __func__);
+		k_thread_suspend((struct k_thread *)this_thread);
 		LOG_INF("%s(): awake", __func__);
+
 		LOG_INF("device_action[%d].value: %d, device_action[%d].name: %s", action_index,
 			device_action[action_index].value, action_index,
 			device_action[action_index].name);
@@ -90,65 +115,63 @@ static void pm_thread(void *this_thread, void *p2, void *p3)
 			     pm_device_runtime_is_enabled(&devices[ii]))) {
 				continue;
 			}
-			if (!device_action_support[ii]
+			if (!device_info[ii]
 				     .action[device_action[action_index].value]
-				     .discovered) {
+				     .probed) {
 				LOG_INF("%s(): initial call to pm_device_action_run(%s, %s)",
 					__func__, devices[ii].name,
 					device_action[action_index].name);
 				ret = pm_device_action_run(&devices[ii],
 							   device_action[action_index].value);
-				if (-ENOSYS == ret) {
-					device_action_support[ii].action[PM_DEVICE_ACTION_SUSPEND] =
-					device_action_support[ii].action[PM_DEVICE_ACTION_RESUME] =
-					device_action_support[ii].action[PM_DEVICE_ACTION_TURN_OFF] =
-					device_action_support[ii].action[PM_DEVICE_ACTION_TURN_ON] =
-						(struct action){.discovered = true, .supported = false};
-				} else {
-					device_action_support[ii]
-						.action[device_action[action_index].value]
-						.discovered = true;
-					device_action_support[ii]
-						.action[device_action[action_index].value]
-						.supported = -ENOTSUP != ret;
+				switch (ret) {
+				case 0:
+				case -EALREADY:
+					device_info[ii]
+					   .action[device_action[action_index].value] =
+					   (struct action){.probed = true, .supported = true};
+					break;
+				case -ENOSYS:
+					device_info[ii].action[PM_DEVICE_ACTION_SUSPEND] =
+					device_info[ii].action[PM_DEVICE_ACTION_RESUME] =
+					device_info[ii].action[PM_DEVICE_ACTION_TURN_OFF] =
+					device_info[ii].action[PM_DEVICE_ACTION_TURN_ON] =
+						(struct action){.probed = true, .supported = false};
+					break;
+				case -ENOTSUP:
+					device_info[ii]
+					   .action[device_action[action_index].value] =
+					   (struct action){.probed = true, .supported = false};
+					break;
+				default:
+					// assert(0 != ret);
+					__ASSERT(0 != ret, "Unexpected return value: %d (%s)", ret,
+						 strerror_extended(ret));
+					break;
 				}
-			} else if (device_action_support[ii]
+			} else if (device_info[ii]
 					   .action[device_action[action_index].value]
 					   .supported) {
-				LOG_INF("%s(): subsequent call to pm_device_action_run(%s, %s)",
-					__func__, devices[ii].name,
-					device_action[action_index].name);
+				// LOG_INF("%s(): subsequent call to pm_device_action_run(%s, %s)",
+				// 	__func__, devices[ii].name,
+				// 	device_action[action_index].name);
 				ret = pm_device_action_run(&devices[ii],
 							   device_action[action_index].value);
 			} else {
 				continue;
 			}
-			/*
-			if (-ENOSYS == ret) {
-				pm_device_state_lock(&devices[ii]);
-				if (!pm_device_state_is_locked(&devices[ii])) {
-					LOG_ERR("Device %s did not enter %s state (%d)",
-						devices[ii].name, device_action[action_index].name,
-						ret);
-				}
-			}
-			*/
 
-			pm_device_state_get(&devices[ii], &pm_device_state);
+			(void) pm_device_state_get(&devices[ii], &pm_device_state);
 
 			if (ret) {
-				LOG_WRN("%s(): %s call status: %d, device state: %s", __func__,
-					devices[ii].name, ret,
+				LOG_WRN("%s(): %s call status: %d (%s), device state: %s", __func__,
+					devices[ii].name, ret, strerror_extended(ret),
 					pm_device_state_str(pm_device_state));
 			} else {
-				LOG_INF("%s(): %s call status: %d, device state: %s", __func__,
-					devices[ii].name, ret,
+				LOG_INF("%s(): %s call status: %d (%s), device state: %s", __func__,
+					devices[ii].name, ret, strerror_extended(ret),
 					pm_device_state_str(pm_device_state));
 			}
 		}
-
-		LOG_INF("%s(): asleep\n", __func__);
-		k_thread_suspend((struct k_thread *)this_thread);
 	}
 }
 
@@ -158,7 +181,7 @@ void main(void)
 	k_thread_create(
 		/* Thread ID and stack specifics */
 		&thread_id, thread_stack, K_THREAD_STACK_SIZEOF(thread_stack),
-		/* Thread function address and (3) optional parameters */
+		/* Thread entry point function and (3) option parameters */
 		pm_thread, &thread_id, NULL, NULL,
 		/* Thread priority, options, and scheduling delay */
 		K_PRIO_COOP(5), K_INHERIT_PERMS, K_NO_WAIT);
