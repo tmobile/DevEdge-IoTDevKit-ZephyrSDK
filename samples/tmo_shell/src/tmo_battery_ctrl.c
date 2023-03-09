ad996bc7c336406abac00d0305e83d1ae8f0df8c
John Lange <John.Lange2@T-Mobile.com>
2023-03-09 03:54:59 -0700

    Improve battery charge/discharge tables
    This improves the battery charge/discharge tables
    
    Signed-off-by: John Lange <John.Lange2@T-Mobile.com>

--- samples/tmo_shell/src/tmo_battery_ctrl.c
+++ samples/tmo_shell/src/tmo_battery_ctrl.c
@@ -16,37 +16,12 @@
 
 #include "tmo_battery_ctrl.h"
 
-#define TABLE_LEN 11
+#define BATTERY_TABLE_LEN 15
 #define TABLE_VOLTAGE 0
 #define TABLE_PERCENT 1
 
-// 100% value based on actual new battery measurement
-static float battery_discharging_tbl [TABLE_LEN][2] = {
-	{4.145, 100.0 },
-	{4.072, 90.0  },
-	{3.992, 80.0  },
-	{3.923, 70.0  },
-	{3.858, 60.0  },
-	{3.806, 50.0  },
-	{3.775, 40.0  },
-	{3.752, 30.0  },
-	{3.729, 20.0  },
-	{3.686, 10.0  },
-	{3.632,  0.0  }};
-
-// 100% value based on actual new battery measurement
-static float battery_charging_tbl [TABLE_LEN][2] = {
-	{4.175, 100.0 },
-	{4.130, 90.0  },
-	{4.071, 80.0  },
-	{4.005, 70.0  },
-	{3.934, 60.0  },
-	{3.886, 50.0  },
-	{3.853, 40.0  },
-	{3.833, 30.0  },
-	{3.800, 20.0  },
-	{3.741, 10.0  },
-	{3.597,  0.0  }};
+extern float battery_discharging_tbl [BATTERY_TABLE_LEN][2];
+extern float battery_charging_tbl [BATTERY_TABLE_LEN][2];
 
 bool is_battery_charging()
 {
@@ -61,7 +36,7 @@ bool is_battery_charging()
 
 float get_remaining_capacity(float battery_voltage)
 {
-	float (*voltage_capacity_table)[TABLE_LEN][2];
+	float (*voltage_capacity_table)[BATTERY_TABLE_LEN][2];
 	float high_voltage = 0.0;
 	float low_voltage = 0.0;
 	uint32_t i;
@@ -77,14 +52,14 @@ float get_remaining_capacity(float battery_voltage)
 		return 100.0;
 	}
 
-	if (battery_voltage <= (*voltage_capacity_table)[TABLE_LEN - 1][TABLE_VOLTAGE]) {
+	if (battery_voltage <= (*voltage_capacity_table)[BATTERY_TABLE_LEN - 1][TABLE_VOLTAGE]) {
 		return 0.0;
 	}
 
 	// find the table percentage entries that correspond
 	// to the voltage and interpolate between the two
 
-	for (i = 1; i < TABLE_LEN; i++) {
+	for (i = 1; i < BATTERY_TABLE_LEN; i++) {
 		if (battery_voltage > (*voltage_capacity_table)[i][TABLE_VOLTAGE]) {
 			high_voltage = (*voltage_capacity_table)[i - 1][TABLE_VOLTAGE];
 			low_voltage = (*voltage_capacity_table)[i][TABLE_VOLTAGE];
@@ -92,7 +67,7 @@ float get_remaining_capacity(float battery_voltage)
 		}
 	}
 
-	if ((i >= TABLE_LEN) || (high_voltage == low_voltage)) {
+	if ((i >= BATTERY_TABLE_LEN) || (high_voltage == low_voltage)) {
 		return 0.0;
 	}
 
